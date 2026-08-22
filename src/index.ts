a3ceb7a050a9f119fcfb19d6f0227e1bd2229890
nadav <nadavx96@walla.com>
2026-08-22 13:16:48 +0300

    added .env and validation via zod

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 import app from "./app";
+import env from "./config/env";
 
-app.listen(3000, () => console.log("server running on port 3000"));
+app.listen(env.PORT, () => console.log(`server running on port ${env.PORT}`));
