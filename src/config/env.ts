2777298d791e0918d50886283621e507ab2fd25c
nadav <nadavx96@walla.com>
2026-08-23 15:55:38 +0300

    added jwt token generation and updated env file

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,8 +4,8 @@ import "dotenv/config";
 const envSchema = z.object({
   PORT: z.coerce.number().default(3000),
   DATABASE_URL: z.string().min(1, "DATABASE_URL is required."),
-  JWT_ACCESS_SECRET: z.string().min(1, "JWT_ACCESS_SECRET is required"),
-  JWT_REFRESH_SECRET: z.string().min(1, "JWT_REFRESH_SECRET is required"),
+  JWT_ACCESS_SECRET: z.string().min(1, "JWT_ACCESS_SECRET is required."),
+  JWT_REFRESH_SECRET: z.string().min(1, "JWT_REFRESH_SECRET is required."),
   JWT_ACCESS_EXPIRY: z.string().default("15m"),
   JWT_REFRESH_EXPIRY: z.string().default("7d"),
 });
