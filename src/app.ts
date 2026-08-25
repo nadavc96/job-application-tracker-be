86fb75296c7f68d5e60acc4c91b4fc684eb4ffa4
nadav <nadavx96@walla.com>
2026-08-25 16:54:35 +0300

    add cookie parser

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,14 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/auth-routes";
+import applicationRouter from "./routes/application-routes";
 
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/auth", authRouter);
+app.use("/api", applicationRouter);
 
 export default app;
