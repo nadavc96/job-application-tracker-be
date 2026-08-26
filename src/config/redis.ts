47e0261a87ffe12da690b913fa0b79f4f97e38b3
nadav <nadavx96@walla.com>
2026-08-26 15:47:54 +0300

    added redis connection to .env and index

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -7,6 +7,7 @@ redis.on("error", (err) => console.error("Redis Client Error", err));
 
 export async function connectRedis() {
   await redis.connect();
+  console.log("Redis connected.");
 }
 
 export default redis;
