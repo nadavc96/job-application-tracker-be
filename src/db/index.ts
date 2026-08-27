0a884aec4746077a8da93289563764a560f89a04
nadav <nadavx96@walla.com>
2026-08-27 11:03:58 +0300

    added connect to postgres function

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,3 +4,8 @@ import env from "../config/env";
 export const pool = new Pool({
   connectionString: env.DATABASE_URL,
 });
+
+export async function connectPostgres() {
+  await pool.query(`SELECT 1`);
+  console.log("Postgres connected.");
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import app from "./app";
 import env from "./config/env";
 import { connectRedis } from "./config/redis";
+import { connectPostgres } from "./db";
 
 async function start() {
   try {
+    await connectPostgres();
     await connectRedis();
 
     app.listen(env.PORT, () =>
