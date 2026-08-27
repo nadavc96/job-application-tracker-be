4e73609e8fdfed31edf82a4a8a148859785954b1
nadav <nadavx96@walla.com>
2026-08-27 11:06:07 +0300

    added postgres error function

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,12 @@
 import { Pool } from "pg";
 import env from "../config/env";
+import { error } from "node:console";
 
 export const pool = new Pool({
   connectionString: env.DATABASE_URL,
 });
+
+pool.on("error", (err) => console.log("Postgres client error", err));
 
 export async function connectPostgres() {
   await pool.query(`SELECT 1`);
