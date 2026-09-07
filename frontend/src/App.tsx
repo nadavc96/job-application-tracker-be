22bf55acff92d1c2b0a09429e6d95ce360168098
nadav <nadavx96@walla.com>
2026-09-07 12:10:01 +0300

    added routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
+import { Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./context/auth-provider";
-import SignInPage from "@/pages/sign-in-page";
+// import SignInPage from "@/pages/sign-in-page";
 import SignUpPage from "@/pages/sign-up-page";
+import LoginPage from "./pages/login-page";
 
 function App() {
   return (
     <AuthProvider>
-      <SignUpPage />
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<SignUpPage />} />
+      </Routes>
     </AuthProvider>
   );
 }
